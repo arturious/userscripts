bbff5c67fb55b0ddc1095598ba6c77b7f61ae2f8
arturious <arturbakhtygereyev@gmail.com>
2026-06-16 19:17:45 +0500

    chore: rename script to youtube and change name in meta

--- dist/youtube.user.js
+++ dist/youtube.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         YouTube Premium Custom Font
+// @name         YouTube
 // @namespace    my-userscripts
 // @version      1.0.0
 // @author       Artur Bakhtygereyev
-// @description  Replaces YouTube Premium logo text with custom styled text using Japan Daisuke font
+// @description  Changes YouTube font to JetBrains Mono globally and inside shadow roots
 // @match        https://www.youtube.com/*
 // @match        https://youtube.com/*
 // @grant        GM_addStyle
