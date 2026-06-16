d1ccd559bb17564c9276a905b2a312d45c6ab06c
arturious <arturbakhtygereyev@gmail.com>
2026-06-17 02:46:47 +0500

    feat: enhance YouTube styling with additional CSS rules for layout and visibility

--- src/youtube/index.ts
+++ src/youtube/index.ts
@@ -9,6 +9,124 @@ const STYLE_CONTENT = `
   * {
     font-family: 'JetBrains Mono', monospace !important;
   }
+
+  /* yt-early New To You chip */
+  #chips {
+    display: flex !important;
+  }
+
+  #chips > yt-chip-cloud-chip-renderer:nth-last-child(1) {
+    order: 1 !important;
+    margin-right: 12px !important;
+  }
+
+  #chips > yt-chip-cloud-chip-renderer:not(:nth-last-child(1)) {
+    order: 2 !important;
+  }
+
+  #chips > yt-chip-cloud-chip-renderer:nth-child(1) {
+    order: 1 !important;
+  }
+
+  /* yt-Compact feed */
+  ytd-rich-item-renderer {
+    margin-bottom: 15px !important;
+    [rendered-from-rich-grid] {
+      --ytd-rich-item-row-usable-width: calc(100% - var(--ytd-rich-grid-gutter-margin) * 1) !important;
+    }
+
+    #metadata.ytd-video-meta-block {
+      flex-direction: row !important;
+
+      #metadata-line span:nth-child(3) {
+        height: 1em !important;
+        margin-left: 1em !important;
+      }
+    }
+  }
+
+  ytd-rich-grid-media {
+    border-radius: 1.2em;
+    height: 100% !important;
+
+    ytd-menu-renderer #button {
+      opacity: 0 !important;
+      transition: opacity 0.3s ease-in-out !important;
+    }
+    :hover ytd-menu-renderer #button {
+      opacity: 1 !important;
+    }
+  }
+
+  /* yt-no distractions */
+  #contents ytd-rich-section-renderer {
+    display: none !important;
+  }
+
+  ytd-merch-shelf-renderer,
+  .annotation {
+    display: none !important;
+  }
+
+  /* yt-better captions */
+  .caption-window {
+    backdrop-filter: blur(10px) brightness(70%) !important;
+    border-radius: 1em !important;
+    padding: 1em !important;
+    box-shadow: #0008 0 0 20px !important;
+    width: fit-content !important;
+  }
+
+  .ytp-caption-segment {
+    background: none !important;
+  }
+
+  /* yt-fix new feed layout */
+  ytd-rich-item-renderer[rendered-from-rich-grid] {
+    @media only screen and (min-width: 1400px) {
+      --ytd-rich-grid-items-per-row: 4 !important;
+
+      @media only screen and (min-width: 1700px) {
+        --ytd-rich-grid-items-per-row: 5 !important;
+
+        @media only screen and (min-width: 2180px) {
+          --ytd-rich-grid-items-per-row: 6 !important;
+        }
+      }
+    }
+  }
+
+  ytd-rich-item-renderer[is-in-first-column=""] {
+    margin-left: calc(var(--ytd-rich-grid-item-margin) / 2) !important;
+  }
+
+  #contents {
+    padding-left: calc(var(--ytd-rich-grid-item-margin) / 2 + var(--ytd-rich-grid-gutter-margin)) !important;
+  }
+
+  /* yt-New player blur */
+  .ytp-left-controls {
+    .ytp-play-button,
+    .ytp-volume-area,
+    .ytp-time-display.notranslate > span,
+    .ytp-chapter-container > button,
+    .ytp-prev-button,
+    .ytp-next-button {
+      backdrop-filter: blur(5px) !important;
+      background-color: #0001 !important;
+    }
+  }
+
+  .ytp-right-controls,
+  .ytp-time-wrapper {
+    backdrop-filter: blur(5px) !important;
+    background-color: #0001 !important;
+  }
+
+  .ytp-popup {
+    backdrop-filter: blur(10px) !important;
+    background-color: #0007 !important;
+  }
 `;
 
 // 1. Внедряем глобально в head документа с импортом шрифта
